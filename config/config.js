27818297a094f51497b3f746be8e9991d1bdfea7
jihwchoi <jihwchoi@ebay.com>
2017-03-14 15:20:58 +0900

    rr

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 exports = module.exports = {
   rootPath: path.normalize(__dirname + '/..'),
-  port: 8989,
+  port: 8080,
   env: process.env.NOVA_ENV,
-  ip: process.env.NOVA_ENV === 'real' ? '172.30.161.173:8989' : 'localhost:8989'
+  ip: process.env.NOVA_ENV === 'real' ? '172.30.161.97:8080' : 'localhost:8989'
 };
